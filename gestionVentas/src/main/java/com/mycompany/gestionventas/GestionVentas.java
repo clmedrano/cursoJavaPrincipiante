54d158d3257b6a47acfb7d2ded6bba29ee552374
Carlos <carlos.sist.2019@gmail.com>
2025-09-21 22:12:56 -0400

    Agrego proyecto gestionVentas como subcarpeta

---
+++ gestionVentas/src/main/java/com/mycompany/gestionventas/GestionVentas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestionventas;
+
+/**
+ *
+ * @author hp
+ */
+public class GestionVentas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
